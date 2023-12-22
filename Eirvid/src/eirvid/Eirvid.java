7dfaba85570e92093dff9b9a7e5f7341d65d41cd
Tumurtulga <2021284@student.cct.ie>
2023-12-22 01:00:50 +0000

    classes

--- Eirvid/src/eirvid/Eirvid.java
+++ Eirvid/src/eirvid/Eirvid.java
@@ -16,6 +16,11 @@ public class Eirvid {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(""
+                + "asdasd"
+                + "asdasd"
+                + "asdasd"
+                + "");
     }
     
 }

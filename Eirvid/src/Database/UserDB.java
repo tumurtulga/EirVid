893f78c7c09958f3967304dad5d9578feae172ff
Tumurtulga <2021284@student.cct.ie>
2023-12-22 22:22:57 +0000

    Update UserDB.java

--- Eirvid/src/Database/UserDB.java
+++ Eirvid/src/Database/UserDB.java
@@ -135,129 +135,125 @@ public class UserDB extends ConnectionDB {
         System.out.println("Your choice: ");
 
         boolean quit = false;
-//        while (!quit) {
-//            boolean saver = false;
-//            try {
-//                TwoVariables two;
-//                ThreeVariables three;
-//                String userInput = "";
-//                String solution = "";
-//                Scanner sc = new Scanner(System.in);
-//                int input = sc.nextInt();
-//                switch (input) {
-//                    case 1:
-//                        userInput = "";
-//                        for (int i = 0; i < 2; i++) {
-//                            System.out.print("Enter equation #" + (i + 1) + ": ");
-//                            if (i != 0) {
-//                                userInput += ", ";
-//                            }
-//                            userInput = userInput + sc.next().trim();
-//                        }
-//                        two = new TwoVariables(userInput);
-//                        solution = "(x, y) " + Arrays.toString(two.solveTwo());
-//                        System.out.println(solution);
-//                        saver = true;
-//                        break;
-//                    case 2:
-//                        userInput = "";
-//                        for (int i = 0; i < 3; i++) {
-//                            System.out.print("Enter equation #" + (i + 1) + ": ");
-//                            if (i != 0) {
-//                                userInput += ", ";
-//                            }
-//                            userInput = userInput + sc.next().trim();
-//                        }
-//                        three = new ThreeVariables(userInput);
-//                        solution = "(x, y, z) " + Arrays.toString(three.solveThree());
-//                        System.out.println(solution);
-//                        saver = true;
-//                        break;
-//                    case 3:
-//                        menuUserChoice();
-//                        quit = true;
-//                        sc.close();
-//                        break;
-//                    default:
-//                        System.out.println("WRONG INPUT, TRY AGAIN");
-//                        break;
-//                }
-//                if (saver) {
-//                    try {
-//                        Connection con = DriverManager.getConnection(db_url, db_username, db_password);
-//                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-//                        Date date = new Date();
-//                        PreparedStatement insert = con.prepareStatement("INSERT INTO equation_data (username, equation, solution, date) VALUES ("
-//                                + "'" + logUser.getUsername() + "', '" + userInput + "', '" + solution + "', '" + formatter.format(date) + "')"
-//                        );
-//                        insert.executeUpdate();
-//                        System.out.println("EQUATION INSERTED TO THE DATABASE");
-//                    } catch (SQLException e) {
-//                        e.printStackTrace();
-//                    }
-//                    saver = false;
-//                }
-//            } catch (InputMismatchException ex) {
-//                System.out.println("Bad Input!!! Must be an integer");
-//            }
-//        }
-//    }
-//
-//    private void accessEquation() {
-//
-//        try {
-//            Connection con = DriverManager.getConnection(db_url, db_username, db_password);
-//            PreparedStatement access = con.prepareStatement("SELECT "
-//                    + "equation_data.id, "
-//                    + "user_data.username, "
-//                    + "equation_data.equation, "
-//                    + "equation_data.solution, "
-//                    + "equation_data.date FROM equation_data INNER JOIN user_data ON "
-//                    + "equation_data.username=user_data.username WHERE user_data.id='" + logUser.getId() + "';");
-//            access.execute();
-//            ResultSet rs = access.executeQuery();
-//            System.out.format("| %-2s | %-15s | %-40s | %-40s | %-15s |%n", "id", "username", "equation", "solution", "date");
-//            while (rs.next()) {
-//                int dbid = rs.getInt("id");
-//                String dbusername = rs.getString("username");
-//                String dbequation = rs.getString("equation");
-//                String dbsolution = rs.getString("solution");
-//                String dbdate = rs.getString("date");
-//                System.out.format("| %-2s | %-15s | %-40s | %-40s | %-15s |%n",
-//                        dbid, dbusername, dbequation, dbsolution, dbdate);
-//            }
-//            access.close();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public void createUserTable() throws ClassNotFoundException {
-//        try {
-//            Connection con = DriverManager.getConnection(db_url, db_username, db_password);
-//            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS user_data ("
-//                    + "id int NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-//                    + "email varchar (255),"
-//                    + "username varchar(255),"
-//                    + "password varchar(255),"
-//                    + "firstname varchar(255),"
-//                    + "lastname varchar(255),"
-//                    + "level varchar(55));"
-//            );
-//            create.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//
-//        }
+        while (!quit) {
+            boolean saver = false;
+            try {
+                TwoVariables two;
+                ThreeVariables three;
+                String userInput = "";
+                String solution = "";
+                Scanner sc = new Scanner(System.in);
+                int input = sc.nextInt();
+                switch (input) {
+                    case 1:
+                        userInput = "";
+                        for (int i = 0; i < 2; i++) {
+                            System.out.print("Enter equation #" + (i + 1) + ": ");
+                            if (i != 0) {
+                                userInput += ", ";
+                            }
+                            userInput = userInput + sc.next().trim();
+                        }
+                        two = new TwoVariables(userInput);
+                        solution = "(x, y) " + Arrays.toString(two.solveTwo());
+                        System.out.println(solution);
+                        saver = true;
+                        break;
+                    case 2:
+                        userInput = "";
+                        for (int i = 0; i < 3; i++) {
+                            System.out.print("Enter equation #" + (i + 1) + ": ");
+                            if (i != 0) {
+                                userInput += ", ";
+                            }
+                            userInput = userInput + sc.next().trim();
+                        }
+                        three = new ThreeVariables(userInput);
+                        solution = "(x, y, z) " + Arrays.toString(three.solveThree());
+                        System.out.println(solution);
+                        saver = true;
+                        break;
+                    case 3:
+                        menuUserChoice();
+                        quit = true;
+                        sc.close();
+                        break;
+                    default:
+                        System.out.println("WRONG INPUT, TRY AGAIN");
+                        break;
+                }
+                if (saver) {
+                    try {
+                        Connection con = DriverManager.getConnection(db_url, db_username, db_password);
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+                        Date date = new Date();
+                        PreparedStatement insert = con.prepareStatement("INSERT INTO equation_data (username, equation, solution, date) VALUES ("
+                                + "'" + logUser.getUsername() + "', '" + userInput + "', '" + solution + "', '" + formatter.format(date) + "')"
+                        );
+                        insert.executeUpdate();
+                        System.out.println("EQUATION INSERTED TO THE DATABASE");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    saver = false;
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("Bad Input!!! Must be an integer");
+            }
+        }
     }
 
     private void rentMovie() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
 
     private void rentedMovie() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            Connection con = DriverManager.getConnection(db_url, db_username, db_password);
+            PreparedStatement access = con.prepareStatement("SELECT "
+                    + "movie_data.id, "
+                    + "user_data.username, "
+                    + "movie_data.original_title, "
+                    + "movie_data.original_language, "
+                    + "movie_data.price, "
+                    + "movie_data.date FROM movie_data INNER JOIN user_data ON "
+                    + "movie_data.username=user_data.username WHERE user_data.id='" + logUser.getId() + "';");
+            access.execute();
+            ResultSet rs = access.executeQuery();
+            System.out.format("| %-2s | %-15s | %-40s | %-40s | %-15s |%n", "id", "username", "original_title", "price", "date");
+            while (rs.next()) {
+                int dbid = rs.getInt("id");
+                String dbusername = rs.getString("username");
+                String dboriginal_title = rs.getString("original_title");
+                String dbprice = rs.getString("price");
+                String dbdate = rs.getString("date");
+                System.out.format("| %-2s | %-15s | %-40s | %-40s | %-15s |%n",
+                        dbid, dbusername, dboriginal_title, dbprice, dbdate);
+            }
+            access.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createUserTable() throws ClassNotFoundException {
+        try {
+            Connection con = DriverManager.getConnection(db_url, db_username, db_password);
+            PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS user_data ("
+                    + "id int NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+                    + "email varchar (255),"
+                    + "username varchar(255),"
+                    + "password varchar(255),"
+                    + "firstname varchar(255),"
+                    + "lastname varchar(255),"
+                    + "level varchar(55));"
+            );
+            create.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
     }
 
 }
